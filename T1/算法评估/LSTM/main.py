91eaa4a2400064ed692da26e430e0879b3583a3a
JamesCurtis <jamescurtis@foxmail.com>
2023-04-15 22:20:56 +0800

    chore: update

--- T1/算法评估/LSTM/main.py
+++ T1/算法评估/LSTM/main.py
@@ -27,6 +27,7 @@ from statsmodels.tsa.arima.model import ARIMA
 from statsmodels.graphics.tsaplots import *
 import os
 
+# tf.compat.v1.disable_v2_behavior()
 forceCpu = False
 # 使用CPU
 if forceCpu:
@@ -38,7 +39,7 @@ if forceCpu:
 if not forceCpu:
     physical_devices = tf.config.list_physical_devices('GPU')
     try:
-      tf.config.experimental.set_memory_growth(physical_devices[0], True)
+        tf.config.experimental.set_memory_growth(physical_devices[0], True)
     except:
         print('Invalid device or cannot modify virtual devices once initialized')
 
@@ -80,6 +81,7 @@ def getxy():
     print(f'清洗后数量：{filtered.shape}')
     return filtered.drop(targetKey, axis=1), filtered[targetKey], filtered
 
+
 # 读入数据
 _, _, data = getxy()
 
@@ -106,8 +108,6 @@ for _, group in data.groupby(['场地1', '场地2']):
 
 print(f'dataset数量: {len(dataset)}')
 
-
-
 # 将序列转换成模型可接受的形式
 '''
 `look_back`是一个超参数，它定义了我们在创建时间序列数据集时要考虑多少个时间步。
@@ -125,15 +125,24 @@ for i in range(len(dataset)):
     trainX.append(train_x)
     trainY.append(train_y)
 
-trainX = np.concatenate(trainX)
-trainY = np.concatenate(trainY)
+trainX, trainY = np.concatenate(trainX), np.concatenate(trainY)
+ratio = int(len(trainX) * 0.9)
+trainX, testX, trainY, testY = trainX[:ratio], trainX[ratio:], trainY[:ratio], trainY[ratio:]
 
 print(f'trainX.shape: {trainX.shape}')
+print(f'testX.shape: {testX.shape}')
 print(f'trainY.shape: {trainY.shape}')
-print(trainX[0])
-print(trainY[0])
+print(f'testY.shape: {testY.shape}')
 
-modelPath = 'LSTM.h5'
+modelPath = 'LSTM.1.h5'
+
+
+# 定义自定义指标函数
+def r_square(y_true, y_pred):
+    SS_res = tf.keras.backend.sum(tf.keras.backend.square(y_true - y_pred))
+    SS_tot = tf.keras.backend.sum(tf.keras.backend.square(y_true - tf.keras.backend.mean(y_true)))
+    return (1 - SS_res / (SS_tot + tf.keras.backend.epsilon()))
+
 
 if not os.path.exists(modelPath):
     # 定义和训练LSTM模型
@@ -141,38 +150,84 @@ if not os.path.exists(modelPath):
         tf.keras.layers.LSTM(100, input_shape=(look_back, trainX.shape[2])),
         tf.keras.layers.Dense(1)
     ])
-    model.compile(loss='mse', optimizer='adam')
-    history = model.fit(trainX, trainY, epochs=5000, batch_size=5000)
+
+    metrics = r_square
+    metricsName = metrics.__name__
+    model.compile(loss='mse', optimizer='adam', metrics=[metrics])
+    # epochs=3000
+    history = model.fit(trainX, trainY, validation_data=(testX, testY), epochs=200, batch_size=5000)
     model.save(modelPath)
 
-   # 绘制训练误差曲线
-    loss = history.history['loss']
-    plt.plot(loss)
-    plt.title('Training loss')
-    plt.ylabel('Loss')
-    plt.xlabel('Epoch')
+    # 绘制学习曲线
+    loss = pd.DataFrame({'训练集损失': history.history['loss'], '测试集损失': history.history['val_loss'], })
+    acc = pd.DataFrame(
+        {'训练集准确率': history.history[metricsName], '测试集准确率': history.history[f'val_{metricsName}'], })
 
+    sns.lineplot(loss)
+    plt.xlabel('Epochs')
+    plt.ylabel('MSE')
     # 绘制最后一个点并添加横线
-    last_loss = loss[-1]
-    plt.plot(len(loss)-1, last_loss, marker='o', color='red')
-    plt.axhline(y=last_loss, color='red', linestyle='--')
-
+    last_loss = history.history['loss'][-1]
+    plt.plot(len(history.history['loss']) - 1, last_loss, marker='o', color='darkred')
+    plt.axhline(y=last_loss, color='darkred', linestyle='--')
     # 添加红色直线的 y 值标签
-    plt.text(len(loss)-1, last_loss+5, f'y={last_loss:.5f}', color='red')
-
+    plt.text(len(loss) - 15, last_loss + 1, f'训练集y={last_loss:.4f}', color='darkred')
+    # 绘制最后一个点并添加横线
+    last_loss = history.history['val_loss'][-1]
+    plt.plot(len(history.history['val_loss']) - 1, last_loss, marker='o', color='darkred')
+    plt.axhline(y=last_loss, color='darkred', linestyle='--')
+    # 添加红色直线的 y 值标签
+    plt.text(len(loss) - 15, last_loss + 5, f'测试集y={last_loss:.4f}', color='darkred')
+    plt.tight_layout()
+    plt.savefig('损失率随训练代数的变化.svg')
     plt.show()
+
+    sns.lineplot(acc)
+    plt.xlabel('Epochs')
+    plt.ylabel('R2')
+    # 绘制最后一个点并添加横线
+    last_acc = history.history[metricsName][-1]
+    plt.plot(len(history.history[metricsName]) - 1, last_acc, marker='o', color='darkred')
+    plt.axhline(y=last_acc, color='darkred', linestyle='--')
+    # 添加红色直线的 y 值标签
+    plt.text(len(acc) - 10, last_acc + 0.1, f'训练集y={last_acc:.4f}', color='darkred')
+    # 绘制最后一个点并添加横线
+    last_acc = history.history[f'val_{metricsName}'][-1]
+    plt.plot(len(history.history[f'val_{metricsName}']) - 1, last_acc, marker='o', color='darkred')
+    plt.axhline(y=last_acc, color='darkred', linestyle='--')
+    # 添加红色直线的 y 值标签
+    plt.text(len(acc) - 10, last_acc - 0.1, f'测试集y={last_acc:.4f}', color='darkred')
+    plt.ylim(0, 1)
+    plt.tight_layout()
+    plt.savefig('准确率随训练代数的变化.svg')
+    plt.show()
+
 else:
     # 加载模型并继续训练
-    model = tf.keras.models.load_model(modelPath)
+    model = tf.keras.models.load_model(modelPath, custom_objects={'r_square': r_square})
 
 # 测试模型
 y_pred = model.predict(trainX)
 
 # 绘制预测结果和真实值的比较图
 plt.figure(figsize=(16, 9))
-plt.plot(np.exp(trainY), label='true')
-plt.plot(np.exp(y_pred), label='pred')
+plt.plot(np.exp(trainY), label='真实值')
+plt.plot(np.exp(y_pred), label='预测值')
+plt.ylabel('货量')
+plt.xlabel('数据序号')
 plt.legend()
+plt.tight_layout()
+plt.savefig('绘制预测结果和真实值的比较图.svg')
+plt.show()
+
+# y预测值-x真实值
+sns.scatterplot({'预测值': np.exp(trainY), '真实值': np.exp(y_pred.reshape(len(y_pred)))}, x='真实值', y='预测值')
+plt.axis('equal')
+plt.axis('square')
+_ = plt.plot([-1e10, 1e10], [-1e10, 1e10])
+plt.tight_layout()
+plt.savefig('y预测值-x真实值.svg')
+plt.savefig('y预测值-x真实值.png')
 plt.show()
 
 print('r2', r2_score(np.exp(trainY), np.exp(y_pred)))
@@ -180,8 +235,29 @@ print('mse', mean_squared_error(np.exp(trainY), np.exp(y_pred)))
 print('mae', mean_absolute_error(np.exp(trainY), np.exp(y_pred)))
 print('mape', mean_absolute_percentage_error(np.exp(trainY), np.exp(y_pred)))
 
-# # 使用模型进行预测
-# testX = np.array([[[3, 5, 1], [3, 5, 2], [3, 5, 3], [3, 5, 4], [3, 5, 5], [3, 5, 6]]])
-# predictions = model.predict(testX)
+'''
+敏感性分析
+'''
+explainerData = np.copy(trainX)
+np.random.shuffle(trainX)
+explainerData = explainerData[:1000]
+explainer = shap.DeepExplainer(model, explainerData)
+shap_values = explainer.shap_values(explainerData)
+
+# plt.figure(figsize=(8, 6))
+# shap.plots.waterfall(shap_values[0][:, 0, :], show=False)
+# plt.tight_layout()
+# plt.savefig('灵敏度瀑布图.svg')
+# plt.show()
 #
-# print(predictions)
+# plt.figure(figsize=(8, 6))
+# shap.plots.bar(shap_values, show=False)
+# plt.tight_layout()
+# plt.savefig('灵敏度条形图.svg')
+# plt.show()
+
+plt.figure(figsize=(16, 9))
+shap.summary_plot(shap_values[0][:, 0, :], explainerData[:][:, 0, :], data.columns, show=False, )
+plt.tight_layout()
+plt.savefig('灵敏度蜂群图.svg')
+plt.show()
